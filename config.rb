cf4c0dfa7088032f3ae1ee56a9253f26e1fd7a6e
Sam Rayner <sam@terracoding.com>
2016-09-29 11:27:10 +0100

    Add HAML content

--- config.rb
+++ config.rb
@@ -2,6 +2,11 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :haml, { format: :html5, attr_wrapper: '"' }
+
 configure :development do
   activate :livereload
 end
@@ -10,10 +15,6 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 end
-
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
 
 require 'extensions/retina_images'
 activate :retina_images, input_folders: ['assets/images']
